757d5849bae45e5e338a07b1e47b76c378bf2e5f
LeeLaWiki <54634059+TheAngel099@users.noreply.github.com>
2026-09-16 03:45:12 +0000

    Removes temporary maintenance scripts

--- patch_url.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const file = 'src/app/api/process_payment/route.ts';
-let code = fs.readFileSync(file, 'utf8');
-code = code.replace(/const baseUrl = .*/, 'const baseUrl = "https://makibros-test.vercel.app";');
-fs.writeFileSync(file, code);
--- test_token.js
+++
@@ -1,7 +0,0 @@
-const https = require('https');
-
-const data = JSON.stringify({
-  // what goes here?
-});
-
-// We can just query Mercado Pago documentation or examples for "mercadopago yape tokenization"
